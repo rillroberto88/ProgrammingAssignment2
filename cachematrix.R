2fe6f6370d8c932104e92366435dc4c2c2f28c55
Rill Robert <rillroberto88@yahoo.com>
2014-12-07 02:08:09 +0100

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate inverse of a matrix in an optimized way, 
+## by caching the inverse, and the second time it is needed retreive it from cache, 
+## avoiding unneccessary computation.
+## Author: Rill Robert (December, 2017)
 
-## Write a short comment describing this function
-
+## creates a special "matrix", which is an object with getter 
+## and setter methods for the matrix data itself and its inverse.
 makeCacheMatrix <- function(X = matrix()) {
-        inv <- NULL
-        set <- function(Y) {
-                X <<- Y
-                inv <<- NULL
+        inv <- NULL              # inv will be the inverse, initialize with NULL
+        set <- function(Y) {     # reset the matrix and inverse with the superassignment operator
+                X <<- Y          # the new matrix will be Y
+                inv <<- NULL     # the inverse is NULL again (for a new matrix)
         }
-        get <- function() X
-        setInverse <- function(inverse) inv <<- inverse
-        getInverse <- function() inv
-        list(set = set, get = get,
-             setInverse = setInverse,
-             getInverse = getInverse)
+        get <- function() X      # return the current matrix
+        setInverse <- function(inverse) inv <<- inverse   # set the inverse to the parameter
+        getInverse <- function() inv                      # return inverse of current matrix
+        list(set = set, get = get,       # the function returns a list of the 
+             setInverse = setInverse,    #    getter and setter methods
+             getInverse = getInverse)    #    for the matrix and its inverse
 }
 
 
-## Write a short comment describing this function
-
+## calculates the inverse of the special "matrix" object created 
+## with the above function and caches the result.
+## If inverse has already been calculated, get it from the cache 
+## and skips the computation. 
+## Otherwise, calculates inverse and sets stores it in cache.
 cacheSolve <- function(X, ...) {
         ## Return a matrix that is the inverse of 'X'
-        inv <- X$getInverse()
-        if(!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        data <- X$get()
-        inv <- solve(data)
-        X$setInverse(inv)
-        inv
+        inv <- X$getInverse()                   # get inverse of object
+        if(!is.null(inv)) {                     # if inverse was already cached (not NULL)
+                message("getting cached data")  # display message
+                return(inv)                     # and return the inverse
+        }                                       # OTHERWISE
+        data <- X$get()                         # get matrix of the local X object
+        inv <- solve(data)                      # calculate inverse of matix
+        X$setInverse(inv)                       # set the inverse for the object
+        inv                                     # inverse is returned by this function
 }
